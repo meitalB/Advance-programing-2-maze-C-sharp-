4920def5625c9ba90bc9fbe27ee04f705c1dfc0c
meitalB <39822007+meitalB@users.noreply.github.com>
2018-06-02 14:21:22 +0300

    Add files via upload

---
+++ ex1-  MVC server and client/src/ServerProject/ProgramServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using ControllerLib;
+using ModelLib;
+using ViewLib;
+using System.Configuration;
+
+namespace ServerProject
+{   /// <summary>
+    /// this is the main of server
+    /// </summary>
+    class ProgramServer
+    {
+        static void Main(string[] args)
+        {
+            string ip = ConfigurationManager.AppSettings["IP"];
+            string port = ConfigurationManager.AppSettings["Port"];
+            Server server = new Server(int.Parse(port), new ClientHandler(new Controller(new ServerModel())));
+            server.Start();
+            Console.WriteLine("finish Server");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex1-  MVC server and client/src/ViewLib/IClientHandler.cs
@@ -0,0 +1,27 @@
+﻿using ClientLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewLib
+{
+    /// <summary>
+    /// interface of the client handler
+    /// </summary>
+    public interface IClientHandler
+    {
+        /// <summary>
+        /// Handles the client.
+        /// </summary>
+        /// <param name="client">The client.</param>
+        void HandleClient(ClientOfServer client);
+        /// <summary>
+        /// Stops the connetion.
+        /// </summary>
+        //void StopConnetion();
+
+    }
+}
