2895aacd5a1d297fbb39ef1026c7db3cfcff67d3
meitalB <39822007+meitalB@users.noreply.github.com>
2018-06-02 14:41:39 +0300

    Add files via upload

---
+++ ex3-server and client Web app/ex3/src/solution/ClientPtoject/ProgramClient.cs
@@ -0,0 +1,23 @@
+﻿using ClientLib;
+using System.Configuration;
+
+namespace ClientProject
+{
+    class ProgramClient
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            string ip = ConfigurationManager.AppSettings["IP"];
+            string port = ConfigurationManager.AppSettings["Port"];
+            Client client = new Client(ip, int.Parse(port));
+            client.Connect();
+            // Wait for the writing and reading tasks to finish.
+            while (client.ReadingTaskRunning || client.WritingTaskRunning) { }
+            client.Close();
+        }
+    }
+}
