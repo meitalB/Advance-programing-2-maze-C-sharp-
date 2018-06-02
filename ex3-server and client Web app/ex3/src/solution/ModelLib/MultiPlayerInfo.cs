5cb81f3ac6d5ac877c869fa5e953c8c2969526d1
meitalB <39822007+meitalB@users.noreply.github.com>
2018-06-02 14:47:46 +0300

    Add files via upload

---
+++ ex3-server and client Web app/ex3/src/solution/ModelLib/MultiPlayerInfo.cs
@@ -0,0 +1,90 @@
+﻿using ClientLib;
+using MazeLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLib
+{
+    /// <summary>
+    /// multi player object info
+    /// </summary>
+    class MultiPlayerInfo
+    {
+        /// <summary>
+        /// Gets or sets the host.
+        /// </summary>
+        /// <value>
+        /// The host client.
+        /// </value>
+        public ClientOfServer Host
+        {
+            get; set;
+        }
+        /// <summary>
+        /// Gets or sets the guest.
+        /// </summary>
+        /// <value>
+        /// The guest.
+        /// </value>
+        public ClientOfServer Guest
+        {
+            get; set;
+        }
+        /// <summary>
+        /// Gets or sets the maze.
+        /// </summary>
+        /// <value>
+        /// The maze.
+        /// </value>
+        public Maze Maze
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiPlayerInfo"/> class.
+        /// </summary>
+        public MultiPlayerInfo()
+        {
+            Host = null;
+            Guest = null;
+            Maze = null;
+        }
+
+        /// <summary>
+        /// Gets the other player.
+        /// </summary>
+        /// <param name="player">The player.</param>
+        /// <returns>the tcp client</returns>
+        /// <exception cref="System.Exception">the player does not exist</exception>
+        public ClientOfServer GetTheOtherPlayer(ClientOfServer player)
+        {
+            if (Host.Equals(player))
+            {
+                return Guest;
+            }
+
+            if (Guest.Equals(player))
+            {
+                return Host;
+            }
+
+            throw new Exception("the player does not exist");
+        }
+
+        /// <summary>
+        /// Contains the player.
+        /// </summary>
+        /// <param name="player">The player.</param>
+        /// <returns>bool if Contains player</returns>
+        public bool ContainPlayer(ClientOfServer player)
+        {
+            if (Guest.Equals(player) || Host.Equals(player)) { return true; }
+            return false;
+        }
+    }
+}
