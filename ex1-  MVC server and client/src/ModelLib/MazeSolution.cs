4fda5048cc5cf9f9c194777ad760f2ec0c7c7d81
meitalB <39822007+meitalB@users.noreply.github.com>
2018-06-02 14:20:31 +0300

    Add files via upload

---
+++ ex1-  MVC server and client/src/ModelLib/MazeSolution.cs
@@ -0,0 +1,103 @@
+﻿using MazeLib;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLib
+{
+    /// <summary>
+    /// Maze Solution represent the solution 
+    /// </summary>
+    public class MazeSolution
+    {
+        /// <summary>
+        /// Gets or sets the name of the game.
+        /// </summary>
+        /// <value>
+        /// The name of the game.
+        /// </value>
+        public string GameName
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets or sets the solution string.
+        /// </summary>
+        /// <value>
+        /// The solution string.
+        /// </value>
+        public string SolutionString
+        {
+            set; get;
+        }
+        /// <summary>
+        /// Gets or sets the solution.
+        /// </summary>
+        /// <value>
+        /// The solution.
+        /// </value>
+        public List<Position> Solution
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets or sets the evaluated nodes.
+        /// </summary>
+        /// <value>
+        /// The evaluated nodes.
+        /// </value>
+        public int EvaluatedNodes { get; set; }
+
+        /// <summary>
+        /// convert maze solotion To the json.
+        /// </summary>
+        /// <returns></returns>
+        public string ToJSON()
+        {
+            JObject obj = new JObject();
+            obj["Name"] = GameName;
+            obj["Solution"] = FromListToString();
+            obj["NodesEvaluated"] = EvaluatedNodes;
+            return obj.ToString();
+        }
+        /// <summary>
+        /// Froms the json.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <returns></returns>
+        public static MazeSolution FromJSON(string str)
+        {
+            MazeSolution ms = new MazeSolution();
+            JObject obj = JObject.Parse(str);
+            ms.GameName = (string)obj["Name"];
+            ms.SolutionString = (string)obj["Solution"];
+            ms.EvaluatedNodes = (int)obj["NodesEvaluated"];
+            return ms;
+        }
+
+        private string FromListToString()
+        {
+            string solution = "";
+            for (int i = 1; i < Solution.Count; i++)
+            {
+                solution += ComparePosition(Solution[i - 1], Solution[i]).ToString();
+            }
+            return solution;
+        }
+
+        private int ComparePosition(Position p1, Position p2)
+        {
+            if (p2.Row < p1.Row) { return 0; }
+            if (p2.Row > p1.Row) { return 1; }
+            if (p2.Col < p1.Col) { return 2; }
+            return 3; //  if (p2.Col > p1.Col) 
+        }
+
+    }
+}
+
---
+++ ex1-  MVC server and client/src/ModelLib/PlayerDirection.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLib
+{
+    /// <summary>
+    /// save the direction of the player
+    /// </summary>
+    public class PlayerDirection
+    {
+        /// <summary>
+        /// Gets or sets the name of the game.
+        /// </summary>
+        /// <value>
+        /// The name of the game.
+        /// </value>
+        public string GameName
+        {
+            get; set;
+        }
+        /// <summary>
+        /// Gets or sets the move.
+        /// </summary>
+        /// <value>
+        /// The move.
+        /// </value>
+        public string Move
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// transpose To the json.
+        /// </summary>
+        /// <returns>string of the direction</returns>
+        public string ToJSON()
+        {
+            JObject obj = new JObject();
+            obj["Name"] = GameName;
+            obj["Direction"] = Move;
+            return obj.ToString();
+        }
+        /// <summary>
+        /// transpose Froms the json.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <returns>PlayerDirection of the direction</returns>
+        public static PlayerDirection FromJSON(string str)
+        {
+            PlayerDirection pd = new PlayerDirection();
+            JObject obj = JObject.Parse(str);
+            pd.GameName = (string) obj["Name"];
+            pd.Move = (string) obj["Direction"];
+            return pd;
+        }
+    }
+}
