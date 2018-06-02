44a8a78ef2e8e99cdd30dd42807db067b3fe54c2
meitalB <39822007+meitalB@users.noreply.github.com>
2018-06-02 14:17:33 +0300

    Add files via upload

---
+++ ex1-  MVC server and client/src/ClientPtoject/ProgramClient.cs
@@ -0,0 +1,19 @@
+﻿using ClientLib;
+using System.Configuration;
+
+namespace ClientProject
+{
+    class ProgramClient
+    {
+        static void Main(string[] args)
+        {
+            string ip = ConfigurationManager.AppSettings["IP"];
+            string port = ConfigurationManager.AppSettings["Port"];
+            Client client = new Client(ip, int.Parse(port));
+            client.Connect();
+            // Wait for the writing and reading tasks to finish.
+            while (client.ReadingTaskRunning || client.WritingTaskRunning) { }
+            client.Close();
+        }
+    }
+}
